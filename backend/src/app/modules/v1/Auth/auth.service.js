dc1ef7aba51aa1f54120d53071c9abe6adc3656d
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2024-08-18 15:20:25 +0600

    sign up work done & install bcyrpt package

--- backend/src/app/modules/v1/Auth/auth.service.js
+++ backend/src/app/modules/v1/Auth/auth.service.js
@@ -1,5 +1,17 @@
-const SignUp = (payload) => {
-  console.log(payload);
+import bcrypt from 'bcrypt';
+import config from '../../../config/index.js';
+import prisma from '../../../utils/prismaClient.js';
+const SignUp = async (payload) => {
+  const bcryptPassword = bcrypt.hashSync(payload.password, Number(config.SALT));
+
+  const data = {
+    password: bcryptPassword,
+    email: payload.email,
+    name: payload.name,
+  };
+  const result = await prisma.user.create({ data: data });
+  const { password, ...rest } = result;
+  return rest;
 };
 
 export const AuthService = {
